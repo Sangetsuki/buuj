a80be870b862be19393b2519cfdc7c1e2b5875c1
luiz <lzeduardo64@gmail.com>
2021-10-23 16:08:36 -0300

    decompile sub_8002764 from bytes

--- src/sub_800274C.c
+++ src/sub_800274C.c
@@ -1,9 +1,16 @@
 #include "gba/gba.h"
 
 void GPIOPortReadEnable();
+void GPIOPortReadDisable();
 
 void sub_800274C()
 {
     GPIOPortReadEnable();
     *((u8*)0x03000A62) = 0;
+}
+
+void sub_8002764()
+{
+    GPIOPortReadDisable();
+    *((u8*)0x03000A62) = 1;
 }
