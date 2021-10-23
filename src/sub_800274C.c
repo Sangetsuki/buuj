cbd3b8b2aaf8afcf3b5e3ce3575e8830c5362800
luiz <lzeduardo64@gmail.com>
2021-10-23 14:00:05 -0300

    document sub_8002FEC and sub_8003000

--- src/sub_800274C.c
+++ src/sub_800274C.c
@@ -1,9 +1,9 @@
 #include "gba/gba.h"
 
-void sub_8002FEC();
+void GPIOPortReadEnable();
 
 void sub_800274C()
 {
-    sub_8002FEC();
+    GPIOPortReadEnable();
     *((u8*)0x03000A62) = 0;
 }
--- src/sub_8002FEC.c
+++ src/sub_8002FEC.c
@@ -1,11 +1,13 @@
 #include "gba/gba.h"
 
-void sub_8002FEC()
+#define GPIOReadPort (u16*)0x080000C8
+
+void GPIOPortReadEnable() // 0x08002FEC
 {
-    *((u16*)0x080000C8) = 1;
+    *GPIOReadPort = 1;
 }
 
-void sub_8003000()
+void GPIOPortReadDisable() // 0x08003000
 {
-    *((u16*)0x080000C8) = 0;   
+    *GPIOReadPort = 0;   
 }
