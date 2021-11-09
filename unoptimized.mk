615d2d9216cb9ef0aafde260290a15e3df5b2bc3
luiz <lzeduardo64@gmail.com>
2021-11-08 22:27:57 -0300

    rename sub_800274C.c

--- unoptimized.mk
+++ unoptimized.mk
@@ -1,11 +1,5 @@
-$(C_BUILDDIR)/sub_800274C.o: $(C_SUBDIR)/sub_800274C.c
-	$(CPP) $(CPPFLAGS) $< -o $(C_BUILDDIR)/sub_800274C.i
-	$(CC1) -mthumb-interwork -Wimplicit -Wparentheses -Werror -O0 -fhex-asm $(C_BUILDDIR)/sub_800274C.i -o $(C_BUILDDIR)/sub_800274C.s
-	echo ".text\n\t.align\t2, 0\n" >> $(C_BUILDDIR)/sub_800274C.s
-	$(AS) $(ASFLAGS) $(C_BUILDDIR)/sub_800274C.s -o $@
-
-$(C_BUILDDIR)/sub_8002FEC.o: $(C_SUBDIR)/sub_8002FEC.c
-	$(CPP) $(CPPFLAGS) $< -o $(C_BUILDDIR)/sub_8002FEC.i
-	$(CC1) -mthumb-interwork -Wimplicit -Wparentheses -Werror -O0 -fhex-asm $(C_BUILDDIR)/sub_8002FEC.i -o $(C_BUILDDIR)/sub_8002FEC.s
-	echo ".text\n\t.align\t2, 0\n" >> $(C_BUILDDIR)/sub_8002FEC.s
-	$(AS) $(ASFLAGS) $(C_BUILDDIR)/sub_8002FEC.s -o $@
+$(C_BUILDDIR)/siirtc.o: $(C_SUBDIR)/siirtc.c
+	$(CPP) $(CPPFLAGS) $< -o $(C_BUILDDIR)/siirtc.i
+	$(CC1) -mthumb-interwork -Wimplicit -Wparentheses -Werror -O0 -fhex-asm $(C_BUILDDIR)/siirtc.i -o $(C_BUILDDIR)/siirtc.s
+	echo ".text\n\t.align\t2, 0\n" >> $(C_BUILDDIR)/siirtc.s
+	$(AS) $(ASFLAGS) $(C_BUILDDIR)/siirtc.s -o $@
